7b2e1a980e8bd0be6bc8a8ebad279de03bd586b5
Pham Thanh <phamthanhfd@gmail.com>
2022-02-16 13:57:40 +0700

    homepage content

---
+++ sites/default/files/php/twig/620c9fdd3d628_page--node.html.twig_lQY2QxYorgNoXARj1PqjkEFJi/Rm7HldAf7LETzTNUnAhbFcxmoCJUexsniRgpwf_N6rA.php
@@ -0,0 +1,141 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/gin/templates/page/page--node.html.twig */
+class __TwigTemplate_14230a2006dca016bea602ba5351b6d127bf8793562ae6d27156a41fdcda39c4 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 42
+        echo "
+<div class=\"page-wrapper__node-edit-form\">
+  ";
+        // line 44
+        $this->loadTemplate("@gin/page/page.html.twig", "themes/gin/templates/page/page--node.html.twig", 44)->display($context);
+        // line 45
+        echo "</div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/gin/templates/page/page--node.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  45 => 45,  43 => 44,  39 => 42,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("{#
+/**
+ * @file
+ * Claro's theme implementation to display a single Drupal page.
+ *
+ * The doctype, html, head, and body tags are not in this template. Instead
+ * they can be found in the html.html.twig template normally located in the
+ * core/modules/system directory.
+ *
+ * Available variables:
+ *
+ * General utility variables:
+ * - base_path: The base URL path of the Drupal installation. Will usually be
+ *   \"/\" unless you have installed Drupal in a sub-directory.
+ * - is_front: A flag indicating if the current page is the front page.
+ * - logged_in: A flag indicating if the user is registered and signed in.
+ * - is_admin: A flag indicating if the user has permission to access
+ *   administration pages.
+ *
+ * Site identity:
+ * - front_page: The URL of the front page. Use this instead of base_path when
+ *   linking to the front page. This includes the language domain or prefix.
+ *
+ * Page content (in order of occurrence in the default page.html.twig):
+ * - node: Fully loaded node, if there is an automatically-loaded node
+ *   associated with the page and the node ID is the second argument in the
+ *   page's path (e.g. node/12345 and node/12345/revisions, but not
+ *   comment/reply/12345).
+ *
+ * Regions:
+ * - page.header: Items for the header region.
+ * - page.pre_content: Items for the pre-content region.
+ * - page.breadcrumb: Items for the breadcrumb region.
+ * - page.highlighted: Items for the highlighted region.
+ * - page.help: Dynamic help text, mostly for admin pages.
+ * - page.content: The main content of the current page.
+ *
+ * @see template_preprocess_page()
+ * @see html.html.twig
+ */
+#}
+
+<div class=\"page-wrapper__node-edit-form\">
+  {% include '@gin/page/page.html.twig' %}
+</div>
+", "themes/gin/templates/page/page--node.html.twig", "C:\\xampp\\htdocs\\entyviohcp-drupal\\themes\\gin\\templates\\page\\page--node.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array("include" => 44);
+        static $filters = array();
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['include'],
+                [],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
