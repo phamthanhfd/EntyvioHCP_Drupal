41b206e99ca882d66e83f7cea11d510f89967c71
Pham Thanh <phamthanhfd@gmail.com>
2022-02-17 22:43:58 +0700

    fix-missing-megamenu

--- themes/custom/entyviohcp/js/entyviohcp.js
+++ themes/custom/entyviohcp/js/entyviohcp.js
@@ -1,20 +1,12 @@
 $( document ).ready(function() {
     $(window).scroll(function () {
-      if ($(this).scrollTop() > 190) {
-        $('header').css("top","-190px");
-      } else {
-        $('header').css("top","0");
-      }
-      if ($(this).scrollTop() > 400) {
+
+      if ($(this).scrollTop() > 500) {
         $('.sticky-bar').css("top","100%");
       } else {
         $('.sticky-bar').css("top","calc(100% - 95px)");
       }
-      if ($(this).scrollTop() < 600) {
-        $('.back-to-top').css("right","-300px");
-      } else {
-        $('.back-to-top').css("right","-40px");
-      }
+
     });
     $(".back-to-top").on("click", function() {
       $("html").scrollTop(0);
@@ -26,4 +18,31 @@ $( document ).ready(function() {
       var img_src = $(this).attr('src');
       $(parent).css('background','url('+img_src+')');
     })
+
+    var varsity = $(".field--name-field-background-head img");
+    var vimg_src = $(varsity).attr('src');
+    $(varsity).closest(".paragraph--type--head-to-head").css('background','url('+vimg_src+')');
+
+    var milestone = $(".field--name-field-milestone-background img");
+    var mimg_src = $(milestone).attr('src');
+    $(milestone).closest(".paragraph--type--milestones").css('background','url('+mimg_src+')');
+
+    $(this).on('wheel', function(event) {
+      if (event.originalEvent.deltaY > 0 ) {
+        if($(window).scrollTop() > 190){
+          $('header').css("top","-190px");
+        }else{
+          $('header').css("top","0");
+        }
+        $('.back-to-top').css("right","-300px");
+      } else {
+        $('header').css("top","0");
+        if ($(window).scrollTop() < 300) {
+          $('.back-to-top').css("right","-300px");
+        } else {
+          $('.back-to-top').css("right","-40px");
+        }
+      }
+    });
+
 });
