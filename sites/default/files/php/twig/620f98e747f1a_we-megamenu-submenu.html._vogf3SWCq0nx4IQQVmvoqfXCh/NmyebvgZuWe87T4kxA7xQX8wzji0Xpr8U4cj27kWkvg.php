13decc878d67104b09144df487346e138f05d74f
Pham Thanh <phamthanhfd@gmail.com>
2022-02-19 21:35:05 +0700

    hp

---
+++ sites/default/files/php/twig/620f98e747f1a_we-megamenu-submenu.html._vogf3SWCq0nx4IQQVmvoqfXCh/NmyebvgZuWe87T4kxA7xQX8wzji0Xpr8U4cj27kWkvg.php
@@ -0,0 +1,102 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* modules/contrib/we_megamenu/templates/we-megamenu-submenu.html.twig */
+class __TwigTemplate_fbe4f7e5d815062ecd41a3e7186ea489b1cee6aedffcc9943ab3738577e2a6fa extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "<div ";
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["attributes"] ?? null), 1, $this->source), "html", null, true);
+        echo ">
+  <div class=\"we-mega-menu-submenu-inner\">
+    ";
+        // line 3
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["row"] ?? null), 3, $this->source), "html", null, true);
+        echo "
+  </div>
+</div>";
+    }
+
+    public function getTemplateName()
+    {
+        return "modules/contrib/we_megamenu/templates/we-megamenu-submenu.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  45 => 3,  39 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("<div {{ attributes }}>
+  <div class=\"we-mega-menu-submenu-inner\">
+    {{ row }}
+  </div>
+</div>", "modules/contrib/we_megamenu/templates/we-megamenu-submenu.html.twig", "C:\\xampp\\htdocs\\entyviohcp-drupal\\modules\\contrib\\we_megamenu\\templates\\we-megamenu-submenu.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array();
+        static $filters = array("escape" => 1);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
