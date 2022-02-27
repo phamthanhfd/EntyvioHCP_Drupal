ca191639d975cb1eef2afc3ad631ee8c42d079f0
Andy Dao <chungdt@techvify.com>
2022-02-27 10:44:57 +0700

    update css

---
+++ sites/default/files/php/twig/621a805e0dcdf_node--entyvioconnect-insu_novNgZkAnGDen6EJZ5BmXFrqu/sNZdpwJPjI4f0-CTKFn9YLMXpvkDv2iLuF_9qvGkPH4.php
@@ -0,0 +1,94 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/custom/entyviohcp/templates/content/node--entyvioconnect-insurance-support--full.html.twig */
+class __TwigTemplate_f5d8b9a6788a196ec77cb7754b3a55db extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->extensions['Drupal\Core\Template\TwigExtension']->attachLibrary("entyviohcp/entyvioconnect-insurance-support-styling"), "html", null, true);
+        echo "
+<h2 style=\"margin-top: 300px\">TEST</h2>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/custom/entyviohcp/templates/content/node--entyvioconnect-insurance-support--full.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  39 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("{{ attach_library('entyviohcp/entyvioconnect-insurance-support-styling') }}
+<h2 style=\"margin-top: 300px\">TEST</h2>
+", "themes/custom/entyviohcp/templates/content/node--entyvioconnect-insurance-support--full.html.twig", "C:\\xampp\\htdocs\\EntyvioHCP_Drupal\\themes\\custom\\entyviohcp\\templates\\content\\node--entyvioconnect-insurance-support--full.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array();
+        static $filters = array("escape" => 1);
+        static $functions = array("attach_library" => 1);
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                ['escape'],
+                ['attach_library']
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
